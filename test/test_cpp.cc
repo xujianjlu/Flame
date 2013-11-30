b35c732b36d91eebfb7d6c8a6b04446081b4ca84
xujian <xujiantjdx@126.com>
2013-11-30 23:44:37 +0800

    fix code style

--- test/test_cpp.cc
+++ test/test_cpp.cc
@@ -1,7 +1,9 @@
+// Copyright [2013] <xujiantjdx@126.com / xujiantjdx@gmail.com>
+
 #include <stdio.h>
 #include <iostream>
 
-#include "test/test.h"
+#include "test/test_cpp.h"
 
 int main(int argc, char **argv) {
   printf("a test \n");
--- test/test_cpp.h
+++ test/test_cpp.h
@@ -1,5 +1,12 @@
+// Copyright [2013] <xujiantjdx@126.com / xujiantjdx@gmail.com>
+
+#ifndef TEST_TEST_CPP_H_
+#define TEST_TEST_CPP_H_
+
 #include <iostream>
 
 void fun() {
   std::cout << "test func" << std::endl;
 }
+
+#endif  // TEST_TEST_CPP_H_
