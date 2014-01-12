4bf8b492647001c7f637f1b146a774e7d47f7f21
xujian <xujiantjdx@126.com>
2014-01-12 19:10:01 +0800

    add base for examples

---
+++ base/README.md
@@ -0,0 +1,20 @@
+base
+====
+
+A powerful C++ base lib.
+
+This part of code is a general infrastructure base/common lib for all projects.  
+It offers you the basic operations with C++, wrap the functions which you can use everywhere,  
+free you from coding the basic modules again and again.    
+
+**How many times you blame**, that why you can't use C++ just like java or python ?  
+**How many times you blame**, that why does C++ forces you to do everything yourself ?  
+even the basic operations like 'string split', 'file operations', and so on?
+
+**Now, you got one !**  
+
+And, for any question, you can feel free to contact with me.  
+Email: xujiantjdx@126.com / xujiantjdx@gmail.com
+
+Referral: [http://code.google.com/p/qa52/source/browse/#svn%2Ftrunk%2Fbase](http://code.google.com/p/qa52/source/browse/#svn%2Ftrunk%2Fbase
+)  
---
+++ base/env.cc
@@ -0,0 +1,40 @@
+// Copyright 2011  Inc. All Rights Reserved.
+// Author: meixuancao@.com (Meixuan Cao)
+
+#include "base/env.h"
+
+#include <map>
+
+#include "base/logging.h"
+
+DEFINE_bool(is_unit_test, false, "");
+
+namespace base {
+
+static std::map<std::string, std::string> g_building_env;
+
+bool is_unit_test() {
+  return FLAGS_is_unit_test;
+}
+
+bool GetBuildingEnv(const std::string &key, std::string *value) {
+  std::map<std::string, std::string>::const_iterator it = g_building_env.find(key);
+  if (it == g_building_env.end())
+    return false;
+  *value = it->second;
+  return true;
+}
+
+void ListBuildingEnvs(std::vector<std::pair<std::string, std::string> > *vec) {
+  vec->clear();
+  for (std::map<std::string, std::string>::const_iterator it = g_building_env.begin();
+       it != g_building_env.end(); ++it) {
+    vec->push_back(make_pair(it->first, it->second));
+  }
+}
+
+void SetBuildingEnv(const std::string &key, const std::string &value) {
+  g_building_env[key] = value;
+}
+
+}  // namespace base
